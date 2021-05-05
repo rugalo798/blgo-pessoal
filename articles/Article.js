6eaa77a9284902bf90dbc2dd8c30689678376594
rugalo798 <tiagovinicius79810@gmail.com>
2021-05-05 18:14:04 -0300

    Relacionando a tabela articles com a tabela categories

--- articles/Article.js
+++ articles/Article.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const connection = require("../database/database");
+const Category = require("../categories/Category");
 
 const Article = connection.define("articles", {
   title: {
@@ -15,5 +16,13 @@ const Article = connection.define("articles", {
       allowNull: false
   }
 });
+
+// Dizendo que uma categoria tem muitos artigos
+Category.hasMany(Article);
+
+// Dizendo que um artigo pertence a uma categoria
+Article.belongsTo(Category);
+
+Article.sync({force: true});
 
 module.exports = Article;
--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const connection = require("./database/database");
 const categoriesController = require("./categories/CategoriesController");
 const articlesController = require("./articles/ArticlesController");
 
+//Importando os models
+const Article = require("./articles/Article");
+const Category = require("./categories/Category")  
+
 //Selecionando a view engine
 app.set("view engine", "ejs");
 
