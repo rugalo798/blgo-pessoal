72c4718e536538dc4b5e1cfae5ba422df7d82545
rugalo798 <tiagovinicius79810@gmail.com>
2021-05-05 18:38:57 -0300

    Criando navbar e formulario de categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/categories", (req, res) => {
-  res.send("Rota de categorias");
-});
-
 router.get("/admin/categories/new", (req, res) => {
-  res.send("Rota de para criar uma nova categoria");
-});
-
+    res.render("admin/categories/new")
+})
 module.exports = router;
