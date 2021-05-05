b8a42ecb57c7762de6eda398efa8457460fb47c7
rugalo798 <tiagovinicius79810@gmail.com>
2021-05-05 18:55:24 -0300

    Configurando o formulario para mandar as categorias pro banco de dados

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const Category = require("./Category");
 const router = express.Router();
+const slugify = require("slugify");
 
 router.get("/admin/categories/new", (req, res) => {
-    res.render("admin/categories/new")
-})
+  res.render("admin/categories/new");
+});
+
+router.post("/categories/save", (req, res) => {
+  var title = req.body.title;
+  if (title != undefined) {
+    Category.create({
+      title: title,
+      slug: slugify(title),
+    }).then(() => {
+      res.redirect("/");
+    });
+  } else {
+    res.redirect("/admin/categories/new");
+  }
+});
+
 module.exports = router;
