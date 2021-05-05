a87b8036ecfc62dda01080c53b39ff49cab7a410
rugalo798 <tiagovinicius79810@gmail.com>
2021-05-05 03:12:54 -0300

    Configurando database, arq. estáticos e partials

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const connection = require("./database/database");
 
 //Selecionando a view engine
 app.set("view engine", "ejs");
 
 //Configurando o body-parser
-app.use(bodyParser.urlencoded({ extends: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//Configurando os arquivos estaticos
+app.use(express.static("public"));
+
+// Conexão com o banco de dados
+connection
+  .authenticate()
+  .then(() => {
+    console.log("Conexão feita com sucesso!");
+  })
+  .catch((err) => {
+    console.log(`Ocorreu um erro: ${err}`);
+  });
 
 //Criando as rotas
 app.get("/", (req, res) => {
